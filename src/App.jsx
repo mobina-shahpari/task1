3a9439a87d390d9611dd842dcafe8c5715896e2a
Mobina <mobinashahpariy@gmail.com>
2024-09-24 18:57:26 +0330

    update porojct

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Header from "./Header/Header.jsx"
+import Main from "./Main/Main.jsx"
+import Footer from "./Footer/Footer.jsx"
+import   "./app.css"
+
+
 const App=()=>{
     return(
         
         <>
-        <h1>hlooooo</h1>
+        <Header/>
+        <Main/>
+        <Footer/>
         </>
     )
 }
